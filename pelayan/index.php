95f1bcc599cec0a3268734c077a5c78cc3373af9
adi andoyo <adi32.dev@gmail.com>
2020-05-11 11:47:47 +0700

    test

--- pelayan/index.php
+++ pelayan/index.php
@@ -52,6 +52,14 @@
 							<a href="menu.php" class="btn btn-success btn-sm">Menu List</a>
 						</div>
 					</div>
+					<div class="card text-center">
+						<div class="card-img">
+							<object data="../assets/images/order.svg" type="image/svg+xml"  style="width: 70px; height: 90px;"></object>
+						</div>
+						<div class="card-body">
+							<a href="#" class="btn btn-success btn-sm" data-toggle="modal" data-target="#pemesanan-modal">New Order</a>
+						</div>
+					</div>
 				</div>		
 			</div>
 		</div>
